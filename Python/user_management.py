4c81d823814ee245e4de5ec6115d3f844ccc7bf4
prajakta-89 <prajaktabhondave2842@gmail.com>
2026-07-22 11:31:18 +0530

    Refactor user management functions and imports

--- Python/user_management.py
+++ Python/user_management.py
@@ -8,7 +8,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Prajakta@567.",
+        password="",
         database="cyber_security_db"
     )
 
